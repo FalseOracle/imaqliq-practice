2a03f5db47c89f1a5487c782eb37dd3689448ad0
Dazzolator <106851297+FalseOracle@users.noreply.github.com>
2024-06-14 13:11:47 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # imaqliq-practice
-## такие пироги
--- php/novnc.php
+++ php/novnc.php
@@ -8,11 +8,11 @@ use ProxmoxVE\Proxmox;
 
 // Create your credentials array
 $credentials = [
-    'hostname' => 'proxy.prox.lan',  // Also can be an IP
+    'hostname' => 'pve.localdomain',  // Also can be an IP
     'username' => 'root',
     'password' => 'P@ssw0rd',
     'realm' => 'pam',
-    'port' => '443'
+    'port' => '8006'
 ];
 
 // Then simply pass your credentials when creating the API client object.
@@ -24,7 +24,7 @@ if ($login = $proxmox->login()) {
     $vmid = 100;
 
     $ticket = $login->getTicket();
-    setcookie("PVEAuthCookie",$ticket, 0, "/", '.prox.lan' );
+    setcookie("PVEAuthCookie",$ticket, 0, "/", 'pve.localdomain' );
     $config = $proxmox->create("/nodes/$node/qemu/$vmid/vncproxy", [
         'websocket' => 1, // Start websocket proxy
     ]);
@@ -34,7 +34,7 @@ if ($login = $proxmox->login()) {
         'port' => $config['data']['port']
     ]);
     // print_r($login);
-    $src_href = 'https://'.$host.'/?console=kvm&novnc=1&node='.$node.'&resize=1&vmid='.$vmid.'&path=api2/json/nodes/'.$node.'/qemu/'.$vmid.'/vncwebsocket/port/'.$config['data']['port'].'/vncticket/'.$config['data']['ticket'];
+    $src_href = 'https://'.$host.':8006/?console=kvm&novnc=1&node='.$node.'&resize=1&vmid='.$vmid.'&path=api2/json/nodes/'.$node.'/qemu/'.$vmid.'/vncwebsocket/port/'.$config['data']['port'].'/vncticket/'.$config['data']['ticket'];
     echo '<iframe src="'.$src_href.'" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>';
 }
 
